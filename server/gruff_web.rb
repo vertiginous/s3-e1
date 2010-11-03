5d512540112dfdc803337a2f2c34becc2756cac8
Gordon Thiesfeld <gthiesfeld@gmail.com>
2010-11-03 15:54:05 -0500

    no need to check if title is nil, just set it

--- server/gruff_web.rb
+++ server/gruff_web.rb
@@ -12,13 +12,15 @@ get '/gruff/:chart.png' do
   chart_type = CHARTS[params[:chart]]
   pass unless chart_type
   halt 500, 'you must include data points' unless params[:data]
+  
   gruff = chart_type.new
-  gruff.title = params[:title] if params[:title]
+  gruff.title = params[:title]
   gruff.send( "theme_#{params[:theme]}" ) if params[:theme]
+  
   params[:data].each{|name,points|
-    points = points.map{|i| i.to_i }
-    gruff.data(name,points)
+    gruff.data(name, points.map{|i| i.to_i })
   }
+
   content_type :png
   gruff.to_blob
 end
